c5954469f72130e53a20083049b7ec55819f4e25
Yuchan Lee <lasuillard@gmail.com>
2025-11-28 06:13:11 +0000

    fix: E2E test failure

--- e2e/options/options.test.ts
+++ e2e/options/options.test.ts
@@ -1,29 +1,29 @@
 import { expect, test } from '^/e2e/fixtures';
 
 test('page title should be extension name', async ({ page, extensionId }) => {
-	await page.goto(`chrome-extension://${extensionId}/src/options.html`);
+	await page.goto(`chrome-extension://${extensionId}/src/options/index.html`);
 	expect(await page.title()).toEqual('Raindrop Sync for Chrome');
 });
 
 test('visit page', async ({ page, extensionId }) => {
-	await page.goto(`chrome-extension://${extensionId}/src/options.html`);
+	await page.goto(`chrome-extension://${extensionId}/src/options/index.html`);
 	await expect(page).toHaveScreenshot();
 });
 
 test('tab Try It', async ({ page, extensionId }) => {
-	await page.goto(`chrome-extension://${extensionId}/src/options.html`);
+	await page.goto(`chrome-extension://${extensionId}/src/options/index.html`);
 	await page.getByText('Try It').click();
 	await expect(page).toHaveScreenshot();
 });
 
 test('tab Settings', async ({ page, extensionId }) => {
-	await page.goto(`chrome-extension://${extensionId}/src/options.html`);
+	await page.goto(`chrome-extension://${extensionId}/src/options/index.html`);
 	await page.getByText('Settings', { exact: true }).click();
 	await expect(page).toHaveScreenshot({ fullPage: true });
 });
 
 test('tab About', async ({ page, extensionId }) => {
-	await page.goto(`chrome-extension://${extensionId}/src/options.html`);
+	await page.goto(`chrome-extension://${extensionId}/src/options/index.html`);
 	await page.getByText('About').click();
 	await expect(page).toHaveScreenshot();
 });
--- e2e/popup/popup.test.ts
+++ e2e/popup/popup.test.ts
@@ -1,11 +1,11 @@
 import { expect, test } from '^/e2e/fixtures';
 
 test('page title should be extension name', async ({ page, extensionId }) => {
-	await page.goto(`chrome-extension://${extensionId}/src/popup.html`);
+	await page.goto(`chrome-extension://${extensionId}/src/popup/index.html`);
 	expect(await page.title()).toEqual('Raindrop Sync for Chrome');
 });
 
 test('visit page', async ({ page, extensionId }) => {
-	await page.goto(`chrome-extension://${extensionId}/src/popup.html`);
+	await page.goto(`chrome-extension://${extensionId}/src/popup/index.html`);
 	await expect(page).toHaveScreenshot();
 });
