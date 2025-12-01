6b70a10a750bad2c3c67e069d75b58bc87a67f62
Yuchan Lee <lasuillard@gmail.com>
2025-12-01 09:20:57 +0000

    feat!: Remove sync button from Bookmarks tab

--- src/options/tabs/Bookmarks.svelte
+++ src/options/tabs/Bookmarks.svelte
@@ -3,16 +3,11 @@
 	import { Button, P } from 'flowbite-svelte';
 	import Tree from '~/components/Tree.svelte';
 	import rd from '~/lib/raindrop';
-	import syncManager from '~/lib/sync';
 
 	let treeNode: utils.tree.TreeNode<generated.Collection | null>;
 
 	const fetchItems = async () => {
 		treeNode = await rd.collection.getCollectionTree();
-	};
-
-	const createBookmarks = async () => {
-		await syncManager.startSync();
 	};
 </script>
 
@@ -20,7 +15,6 @@
 	<P data-testid="description">Bookmarks synchronization settings.</P>
 	<div class="mt-2 space-x-2">
 		<Button outline onclick={fetchItems}>Fetch</Button>
-		<Button outline onclick={createBookmarks}>Synchronize</Button>
 	</div>
 	<div class="mt-2 p-2">
 		{#if treeNode}
