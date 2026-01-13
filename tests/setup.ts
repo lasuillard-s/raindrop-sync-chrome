9e6e75797d4ec3bd9f740f0fa3e769d2f9bab332
Yuchan Lee <lasuillard@gmail.com>
2026-01-13 07:28:51 +0000

    chore(deps-dev): Replace sinon-chrome with vitest mocking APIs

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,16 +1,23 @@
 import { cleanup } from '@testing-library/svelte';
-import * as chrome from 'sinon-chrome';
-import { afterEach, beforeEach, vi } from 'vitest';
+import { afterEach, beforeAll, vi } from 'vitest';
 
-// Chrome API N/A in unit tests
-vi.stubGlobal('chrome', chrome);
-
-beforeEach(() => {
-	vi.mock('~/lib/raindrop/client');
+beforeAll(() => {
+	// Tried to use both sinon-chrome and vitest-chrome, but it seems both are not being
+	// maintained for a while. So opted to just stub the parts we need directly.
+	vi.stubGlobal('chrome', {
+		// Here only provide sane defaults and scaffolding for the parts we use in tests.
+		// Each test can follow pattern like:
+		//
+		// vi.mocked(chrome.bookmarks.getSubTree).mockImplementationOnce(...)
+		//
+		identity: {
+			getRedirectURL: vi.fn(),
+			launchWebAuthFlow: vi.fn()
+		}
+	});
 });
 
 afterEach(() => {
-	chrome.flush();
 	vi.resetAllMocks();
 	cleanup();
 });
