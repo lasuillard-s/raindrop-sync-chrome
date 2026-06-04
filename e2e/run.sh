5cc3112a922e6a5ee7bf51400f57210cb08ed461
Yuchan Lee <lasuillard@gmail.com>
2026-06-05 00:02:35 +0900

    fix: update log file paths and add Playwright version (#298)

--- e2e/run.sh
+++ e2e/run.sh
@@ -7,7 +7,7 @@ set -o nounset
 : '
 Set up the environment for end-to-end testing using Docker Compose.
 
-Logs will be saved to e2e-*.log files.
+Logs will be saved to e2e/run-*.log files.
 '
 
 root_dir="$(realpath "$(dirname "${0}")/..")"
@@ -16,26 +16,24 @@ log_file="${e2e_dir}/run-$(date +"%Y%m%dT%H%M%S").log"
 
 cd "$e2e_dir"
 
-docker compose config >> "$log_file"
+# Get the Playwright version
+if [ -z "${PLAYWRIGHT_VERSION:-}" ]; then
+  export PLAYWRIGHT_VERSION="$(yarn exec --silent -- playwright --version | awk '{print $2}')"
+fi
+echo "Using Playwright version ${PLAYWRIGHT_VERSION}"
 
-# Pull the base images and build the services, while logging the output.
-docker compose pull --ignore-buildable --include-deps >> "$log_file" 2>&1
-docker buildx bake \
-    --file ./docker-compose.yaml \
-    --file ./docker-bake.json \
-    --allow=fs.read=.. \
-    --load \
-    2>&1 | tee --append "$log_file"
+# Print the docker compose configuration for debugging
+docker compose config | tee --append "$log_file"
 
 # Register a cleanup function
 function cleanup() {
     docker compose logs --timestamps >> "$log_file"
-    docker compose down
+    docker compose down | tee --append "$log_file"
 }
 trap cleanup EXIT
 
 # Start the services and wait for them to be healthy
-docker compose up --wait --wait-timeout 600
+docker compose up --wait --wait-timeout 600 | tee --append "$log_file"
 
 # Run the e2e tests, passing specified host environment variables to the container
 pass_envvars=( CI )
