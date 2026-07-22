92436c907bfee4a98c04be3cb0d9694684c7ae88
Yuchan Lee <lasuillard@gmail.com>
2026-07-22 13:07:15 +0000

    Simplify E2E test setup without Docker for maintainability and performance

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,16 @@ export default defineConfig({
 	use: {
 		screenshot: isCI ? 'on' : 'only-on-failure',
 		video: isCI ? 'on' : 'retain-on-failure',
-		trace: isCI ? 'on' : 'on-first-retry'
+		trace: isCI ? 'on' : 'on-first-retry',
+		channel: 'chromium',
+		launchOptions: {
+			args: [
+				'--font-render-hinting=none',
+				'--disable-skia-runtime-opts',
+				'--disable-font-subpixel-positioning',
+				'--disable-lcd-text'
+			]
+		}
 	},
 	testDir: 'e2e',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
